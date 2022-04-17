b8a24292b65cbf6c24ef1e5b7a14082b914cd6d0
Ben Epstein <ben.epstein@wustl.edu>
2022-04-17 18:25:26 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from typing import Optional
 import pandas as pd
 import requests
 import os
+import numpy as np
 
 CWD = os.getcwd()
 url = "https://macrotrends-finance.p.rapidapi.com"
 
-FKR = "financial-key-ratios"
-FCS = "financial-cash-statement"
-FBS = "financial-balance-sheet"
 FIS = "financial-income-statement"
+FBS = "financial-balance-sheet"
+FCS = "financial-cash-statement"
+FKR = "financial-key-ratios"
+
+DATAS = [FIS, FBS, FCS, FKR]
 
 headers = {
     "X-RapidAPI-Host": "macrotrends-finance.p.rapidapi.com",
@@ -25,11 +28,25 @@ all_stocks = pd.concat([nasdaq, nyse])
 
 
 @st.cache
-def get_stock_info(sym: str, data: str) -> Optional[pd.DataFrame]:
-    data_res = requests.get(f"{url}/{data}/{sym.replace('/','.')}", headers=headers)
-    if data_res.status_code != 200:
-        return
-    return pd.DataFrame(data_res.json())
+def get_stock_info(sym: str) -> Optional[pd.DataFrame]:
+    dfs = []
+    for data in DATAS:
+        data_res = requests.get(f"{url}/{data}/{sym.replace('/','.')}", headers=headers)
+        if data_res.status_code != 200:
+            return
+        df = pd.DataFrame(data_res.json())
+        for c, dt in zip(list(df.columns), df.dtypes):
+            if dt == "object":
+                df[c] = df[c].replace("", np.nan)
+        dfs.append(df)
+    
+    df_merged = None
+    for df in dfs:
+        if df_merged is None:
+            df_merged = df
+        else:
+            df_merged = df_merged.merge(df, left_index=True, right_index=True)
+    return df_merged
 
 
 sym = st.selectbox(
@@ -44,22 +61,15 @@ st.write("Gathering Balance Sheet information...")
 
 if sym:
     st.write(sym)
-    data = st.selectbox(
-        'What data would you like?',
-        [FBS, FKR, FCS, FIS]
-    )
-    df = get_stock_info(sym, data)
-    if df is None:
-        st.warning(f"It seems {data} for {sym} is not available. Try another stock.")
-    else:
-        for c, dt in zip(list(df.columns), df.dtypes):
-            if dt == "object":
-                df = df.drop(columns=[c])
 
+    df = get_stock_info(sym)
+    if df is None:
+        st.warning(f"It seems data for {sym} is not available. Try another stock.")
+    else:
         st.download_button(
            "Download",
            df.to_csv(),
-           f"{sym}_{data}.csv",
+           f"{sym}.csv",
            "text/csv",
            key='download-csv'
         )
